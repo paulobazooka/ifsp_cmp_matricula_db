1086ac794f43dec0eb93d9c4f09a3f3c134b760c
paulobazooka <psn.ads.ifsp@gmail.com>
2017-06-12 19:13:47 -0300

    atualização procedimentos

--- procedimentos.sql
+++ procedimentos.sql
@@ -1,78 +1,101 @@
--- Consulta das Turmas/Disciplinas ativas aluno --
-use SisMatricula;
-drop procedure if exists procedure_aluno_turmas_ativas;  -- se existir procedimento, deletar.
-
+-- Inserir novo Aluno
+USE SisMatricula;
+DROP PROCEDURE IF EXISTS procedure_insert_novo_aluno;
 DELIMITER $$
-CREATE PROCEDURE procedure_aluno_turmas_ativas (in codAluno int) -- entrar com o codigo do aluno
+CREATE PROCEDURE procedure_insert_novo_aluno(IN id varchar(20),
+                                             IN nome varchar(75),
+                                             IN sen varchar(50),
+                                             IN data_nas date,
+                                             IN CPF bigint,
+                                             IN ing date,
+                                             IN em varchar(50),
+                                             IN tel1 varchar(15),
+                                             IN tel2 varchar(15))
+
 BEGIN
-select siglaTurma     as 'Turma',
-       nomeDisciplina as 'Disciplina',
-       semestre 	  as 'Semestre',
-       anoTurma 	  as 'Ano'
-from   Disciplina natural join
-       Turma      natural join
-       Matricula  natural join
-       Usuario    natural join
-       TipoUsuario
-where TipoUsuario.codTipoUsuario = 5   -- tipo de usuário aluno
-  AND Usuario.codUsuario = codAluno
-  AND concluido = 0;
-END $$
-DELIMITER ;
-
-call procedure_aluno_turmas_ativas(5);
-
-
-
-
--- Consulta de disciplinas já concluídas --
-use SisMatricula;
-drop procedure if exists procedure_aluno_turmas_concluidas;  -- se existir procedimento, deletar.
-
-DELIMITER $$
-CREATE PROCEDURE procedure_aluno_turmas_concluidas (in codAluno int) -- entrar com o codigo do aluno
-BEGIN
-select t.siglaTurma     as 'Turma',
-       d.nomeDisciplina as 'Disciplina',
-       d.semestre 	  as 'Semestre',
-       t.anoTurma 	  as 'Ano'
-       
-from   Disciplina d natural join
-       Turma      t natural join
-       DisciplinaConcluida dc natural join
-       Usuario  u   natural join
-       TipoUsuario tp
-       
-       
-where  tp.codTipoUsuario = 5
-  and  u.codUsuario = codAluno;
-END $$
-DELIMITER ;
-
-call procedure_aluno_turmas_concluidas(5);
-
-
-
-
--- Consultar dados Aluno --
-use SisMatricula;
-drop procedure if exists procedure_aluno_retorna_dados;
-
-DELIMITER $$
-CREATE PROCEDURE procedure_aluno_retorna_dados(in codAluno int) -- entrar com o codigo do aluno
-BEGIN
-select identificacao 					 as 'Prontuário',
-       nomeUsuario   					 as 'Aluno',
-       date_format(dataNas, '%d/%m/%Y')  as 'Data Nascimento',
-       cpf			                     as 'CPF',
-       date_format(ingresso, '%d/%m/%Y') as 'Ingresso',
-       email		 					 as 'e-mail',
-       telefone1     					 as 'Telefone 1',
-       telefone2     				     as 'Telefone 2'
-from Usuario 
+  
+  START TRANSACTION;
+  INSERT INTO Usuario(identificacao, nomeUsuario, senha, dataNas, cpf, ingresso, 
+                                    email, telefone1, telefone2, codTipoUsuario) VALUES
+                      (id, nome, MD5(sen), data_nas, CPF, ing, em, tel1, tel2, 5);            
+  
+  IF (id = (select identificacao from Usuario ORDER BY codUsuario DESC LIMIT 1)) THEN
+	COMMIT;
+    SELECT true from dual;
+  ELSE
+    ROLLBACK;
+    SELECT false from dual;
+  END IF; 
      
-where codUsuario = codAluno;     
 END $$
-DELIMITER ;
+                                             
+DELIMITER ;       
 
-call procedure_aluno_retorna_dados(6);
+
+
+
+-- alterar aluno geral
+USE SisMatricula;
+DROP PROCEDURE IF EXISTS procedure_update_aluno;
+DELIMITER $$
+CREATE PROCEDURE procedure_update_aluno(IN cod int,										
+									    IN nome varchar(75),										
+										IN data_nas date,																		
+										IN em varchar(50),
+										IN tel1 varchar(15),
+										IN tel2 varchar(15))
+
+BEGIN
+  
+  START TRANSACTION;
+  
+  UPDATE Usuario SET nomeUsuario = nome, dataNas = data_nas, email = em,
+                          telefone1 = tel1, telefone2 = tel2 WHERE codUsuario = cod; 
+                                  
+  IF (nome = (select nome from Usuario where codUsuario = cod) AND
+	  data_nas = (select dataNas from Usuario where codUsuario = cod) AND
+      em = (select email from Usuario where codUsuario = cod) AND
+	  tel1 = (select telefone1 from Usuario where codUsuario = cod) AND
+      tel2 = (select telefone2 from Usuario where codUsuario = cod))        THEN
+      
+	COMMIT;
+    SELECT true from dual;
+    
+  ELSE
+    ROLLBACK;
+    SELECT false from dual;
+  END IF; 
+     
+END $$
+                                             
+DELIMITER ;     
+
+
+
+-- delete usuario geral
+USE SisMatricula;
+DROP PROCEDURE IF EXISTS procedure_delete_usuario;
+DELIMITER $$
+CREATE PROCEDURE procedure_delete_usuario(IN cod int)
+
+BEGIN
+  
+  START TRANSACTION;
+  
+  DELETE FROM Usuario WHERE codUsuario = cod; 
+                                  
+  IF ((select codUsuario from Usuario where codUsuario = cod) is null) THEN      
+ 	  COMMIT;
+      SELECT true from dual;
+    
+  ELSE
+    ROLLBACK;
+    SELECT false from dual;
+  END IF; 
+     
+END $$
+                                             
+DELIMITER ;                                                                                             
+
+
+call procedure_delete_usuario(56);
