fb4b1f3824c9293d6a288261c0f94105ad02ab54
paulobazooka <psn.ads.ifsp@gmail.com>
2017-06-17 23:23:45 -0300

    alteração procedure inserir aluno

--- SisMatricula-DB/PROCS/procedure_insert_aluno.sql
+++ SisMatricula-DB/PROCS/procedure_insert_aluno.sql
@@ -8,8 +8,7 @@ CREATE PROCEDURE procedure_insert_aluno(IN id varchar(20),
                                         IN nome varchar(75),
                                         IN sen varchar(50),
                                         IN data_nas date,
-                                        IN CPF bigint,
-                                        IN ing date,
+                                        IN CPF bigint,                                        
                                         IN em varchar(50),
                                         IN tel1 varchar(15),
                                         IN tel2 varchar(15))
@@ -17,9 +16,9 @@ CREATE PROCEDURE procedure_insert_aluno(IN id varchar(20),
 BEGIN
   
   START TRANSACTION;
-  INSERT INTO Usuario(identificacao, nomeUsuario, senha, dataNas, cpf, ingresso, 
+  INSERT INTO Usuario(identificacao, nomeUsuario, senha, dataNas, cpf, 
                                     email, telefone1, telefone2, codTipoUsuario) VALUES
-                      (id, nome, MD5(sen), data_nas, CPF, ing, em, tel1, tel2, 5);            
+                      (id, nome, MD5(sen), data_nas, CPF, em, tel1, tel2, 5);            
   
   IF (id = (select identificacao from Usuario ORDER BY codUsuario DESC LIMIT 1)) THEN
 	COMMIT;
